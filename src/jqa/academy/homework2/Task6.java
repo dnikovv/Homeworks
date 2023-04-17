7246041753a1c7e2dfc1183b1e29002fec2bc5ce
dnikov <dnikov12@gmail.com>
2023-04-17 20:27:36 +0300

    equals method added

--- src/jqa/academy/homework2/Task6.java
+++ src/jqa/academy/homework2/Task6.java
@@ -7,10 +7,23 @@ public class Task6 {
         int[] arr1 = {1, 2, 3, 4};
         int[] arr2 = {1, 2, 3, 4};
 
-        if (Arrays.equals(arr1, arr2)) {
+        if (equals(arr1, arr2)) {
             System.out.println("The two arrays are equal");
         } else {
             System.out.println("The two arrays are NOT equal");
         }
+
+    }
+
+    public static boolean equals(int[] a, int[] b) {
+        if (a.length != b.length) {
+            return false;
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
