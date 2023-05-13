8e3ddf7aa808a03974f535ba1ce21ba97d230d5a
dnikov <dnikov12@gmail.com>
2023-05-13 12:44:51 +0300

    homework4 refactoring

--- src/jqa/academy/homework4/ComputerPrices.java
+++ src/jqa/academy/homework4/ComputerPrices.java
@@ -4,8 +4,13 @@ public class ComputerPrices {
 
     public static void main(String[] args) {
 
-        Computer comp1 = new Computer(2001,900.50,"Windows");
-        Computer comp2 = new Computer(2008,900.50,"MacOS");
+        Computer comp1 = new Computer(2001, 900.50, "Windows");
+        Computer comp2 = new Computer(2008, 900.50, "MacOS");
+
+        double newPrice = comp1.setPrice(501.20);
+
+        System.out.println("The price of the first computer is: " + comp1.getPrice());
+        System.out.println("The price of the second computer is: " + comp2.getPrice());
 
         int result = comp1.comparePrice(comp2);
         System.out.println("Comparison result:" + " " + result);
