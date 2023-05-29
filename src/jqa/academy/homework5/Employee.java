773f8e3cc3d402f008884488d5fec952082de9c9
dnikov <dnikov12@gmail.com>
2023-05-29 22:11:25 +0300

    homework5 refactor

--- src/jqa/academy/homework5/Employee.java
+++ src/jqa/academy/homework5/Employee.java
@@ -1,14 +1,12 @@
 package jqa.academy.homework5;
 
 public class Employee extends Person {
-
     private double daySalary;
 
     public Employee(String name, int age, boolean isMan, double daySalary) {
         super(name, age, isMan);
         this.daySalary = daySalary;
     }
-
     public double calculateOvertime(double hours) {
         if (getAge() < 18) {
             return 0.0;
@@ -18,7 +16,6 @@ public class Employee extends Person {
             return hours * overtimeRate;
         }
     }
-
     public void showEmployeeInfo() {
         super.showPersonInfo();
         System.out.println("Day Salary: $" + daySalary + " \n----------------");
--- src/jqa/academy/homework5/Student.java
+++ src/jqa/academy/homework5/Student.java
@@ -1,20 +1,17 @@
 package jqa.academy.homework5;
 
-public class Student extends Person{
+public class Student extends Person {
 
     private double score;
-
-    Student(String name, int age, boolean isMan,double score) {
+    Student(String name, int age, boolean isMan, double score) {
         super(name, age, isMan);
         this.score = score;
-        if(score < 2 || score > 6){
+        if (score < 2 || score > 6) {
             System.out.println("Invalid number");
         }
     }
-
     public void showStudentInfo() {
         super.showPersonInfo();
         System.out.println("Score: " + score + "\n----------------");
     }
-
 }
